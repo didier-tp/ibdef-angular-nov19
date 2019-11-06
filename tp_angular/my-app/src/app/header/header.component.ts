25c4bcdddfab13b30cd1870352e159d057886b60
didier-tp <didier@d-defrance.fr>
2019-11-06 12:16:01 +0100

    avec menu deroulant en commentaire dans header.component.ts

--- tp_angular/my-app/src/app/header/header.component.ts
+++ tp_angular/my-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MenuDefinition } from 'src/bs-util/data/MenuDefinition';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,19 @@ export class HeaderComponent implements OnInit {
 
   @Input()
   titre : string = "default title";
+
+  menuDefs : MenuDefinition[] = [
+    { label : "Essentiel" , 
+      children : [
+        { label : "login" , path : "/login" } ,
+        { label : "basic" , path : "/basic" },
+        { divider : true },
+        { label : "welcome" , path : "/welcome" }
+      ]
+    },
+    { label : "welcome" , path : "/welcome" } , 
+    { label : "basic" , path : "/basic" }
+    ];
 
   constructor() { }
 
