09f7c1306815dc54f2c2c9df88dd91a5a834ba30
didier-tp <didier@d-defrance.fr>
2019-11-06 17:32:27 +0100

    pre version deviseService

--- tp_angular/my-app/src/app/common/service/devise.service.ts
+++ tp_angular/my-app/src/app/common/service/devise.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Devise } from '../data/devise';
+import { ResConv } from '../data/resconv';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,27 @@ import { Injectable } from '@angular/core';
 export class DeviseService {
 
   constructor() { }
+
+  private simuTabDevises = [
+      { code : "USD" , name : "dollar" , change : 1.1 },
+      { code : "EUR" , name : "euro" , change : 1 },
+      { code : "GBP" , name : "livre" , change : 0.9 }
+  ];
+
+  //public rechercherToutesDevises() : Promise<Devise[]>{
+  //public rechercherToutesDevises() : Observable<Devise[]>{
+  public rechercherToutesDevises() : Devise[]{
+    return this.simuTabDevises;//simulation temporaire
+  }
+
+  public convertir(source : string,
+                   target:string,
+                   amount : number) : ResConv{
+    //code temporaire (simulation):                     
+    return { source : source , 
+             target : target , 
+             amount : amount ,
+             result : amount * 1.1 };
+  }
+
 }
--- tp_angular/my-app/src/app/conversion/conversion.component.ts
+++ tp_angular/my-app/src/app/conversion/conversion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviseService } from '../common/service/devise.service';
+import { Devise } from '../common/data/devise';
+import { ResConv } from '../common/data/resconv';
 
 @Component({
   selector: 'app-conversion',
@@ -8,8 +10,24 @@ import { DeviseService } from '../common/service/devise.service';
 })
 export class ConversionComponent implements OnInit {
 
+  codeSource : string = "EUR";//select avec [(ngModel)]
+  codeCible : string = "USD";//select avec [(ngModel)]
+  montant : number = 0;//input avec [(ngModel)]
+  resultat : number ; //à afficher via {{}}
+
+  listeDevises : Devise[];//pour option de select
+
+  onConvertir(){
+    let resConv : ResConv = 
+      this.deviseService.convertir(this.codeSource,
+                                this.codeCible,
+                                this.montant);
+      this.resultat = resConv.result;
+  }
+
   constructor(private deviseService : DeviseService) { 
     //injection de dépendance (pour déléguer des appels)
+    this.listeDevises = this.deviseService.rechercherToutesDevises();
   }
 
   ngOnInit() {
